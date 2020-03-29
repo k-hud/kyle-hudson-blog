855cff266c12ba9860964eefd003a0777464da3d
Kyle Hudson <36278207+concept-theory@users.noreply.github.com>
2020-03-29 12:08:44 -0400

    Integrate Netfliy and test

--- src/components/article/index.js
+++ src/components/article/index.js
@@ -7,6 +7,7 @@ const Article = () => (
     <Link to="/" className="c-article">
         <img alt="" src="https://source.unsplash.com/random/800x600" />
         <h2>Kyle Hudson</h2>
+        <p>Consequat duis consequat et consectetur duis cupidatat commodo aliqua nisi ipsum culpa. Amet consectetur aute aliqua in aliqua deserunt mollit velit laborum minim laborum mollit. Mollit do eiusmod reprehenderit dolor sunt excepteur anim. Fugiat nostrud sunt consectetur amet eiusmod dolor qui nisi labore est elit aute excepteur sit. Culpa cillum cillum est in.</p>
     </Link>
 )
 
